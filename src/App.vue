9ed7ea27f3f44b532ec7aaed49bd3f2c6a759bc0
greysalt <richard@163.com>
2017-11-29 21:04:49 +0800

    v1

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style lang="scss">
+*{margin: 0; padding: 0; box-sizing: border-box;}
+body{background: #333; font-size: 13px;}
+a{text-decoration:none;}
+
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="home-container">
+    <div class="entry">
+      <router-link to="/detail/16049">ENTRY</router-link>
+    </div>   
+  </div>
+</template>
+
+<script>
+</script>
+<style lang="scss">
+.home-container{
+  width:200px;
+  height:200px;
+  border:1px solid #fff;
+  border-radius:200px;
+  position:fixed;
+  top:50%;
+  left:50%;
+  transform:translate(-50%,-50%);
+  display:flex;
+  justify-content:center;
+  align-items:center;
+
+  .entry {
+    width: 100px;
+    height: 100px;
+    border-radius: 100px;
+    border:1px solid #fff;
+    text-align:center;
+    &:hover{
+      background:#666;
+    }
+
+    a {
+      color:#fff;
+      line-height:100px;
+      font-size:20px;
+    }
+  }
+} 
+
+
+</style>
